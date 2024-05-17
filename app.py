4deb673e6e88ca5fd7aa23dfe74fa13560cf8212
naka0519 <naka20000519@gmail.com>
2024-05-17 12:49:16 +0900

    update app.py

--- app.py
+++ app.py
@@ -6,14 +6,24 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 
-st.write("hello streamlit")
+# タイトル
+st.write("Suntory beacon data visualization")
 
-xs = np.linspace(0, 10, 100)
-sins = np.sin(xs)
-randoms = np.random.rand(100)
+# データを選択（選択でファイルpath指定）
+# csvを読み込む前提バージョン（事前に抽出が必要）
+# TODO: データベースから取得するように変更
+Floor = st.selectbox("Floor", ["1F", "2F", "3F"])
+User = st.selectbox("User", ["User1", "User2", "User3"])
+
+DataPath = f"./data/{Floor}_{User}.csv"
+DataPath = "./data/test.csv"
+# 可視化
+df = pd.read_csv(DataPath) # index: datatime
+
+x = df.index
+y = df["prediction"]
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=xs, y=sins, name="sin"))
-fig.add_trace(go.Scatter(x=xs, y=randoms, name="random"))
+fig.add_trace(go.Scatter(x=x, y=y, name=User))
 #fig.show()
 st.plotly_chart(fig)
