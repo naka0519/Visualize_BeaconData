408f692d95d7b036602d56213834134fe4b1f875
naka0519 <naka20000519@gmail.com>
2024-05-21 16:32:40 +0900

    update app

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ try:
 
     # predictionごとの総時間
     st.write("Total time of visualization: ", end_time - start_time)
-    st.erite("Total time of room: ", datetime.timedelta(seconds=(df["prediction"]=="room").sum().sum()))
+    st.erite("Total time of room: ", timedelta(seconds=(df["prediction"]=="room").sum().sum()))
 
 
 except:
