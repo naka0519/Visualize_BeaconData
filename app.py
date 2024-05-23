ea84a4697282d820eb3f3a7ae144239ab70feb5e
naka0519 <naka20000519@gmail.com>
2024-05-23 12:24:26 +0900

    fix app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import streamlit as st
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta, time
