871fdea623a412f38a0037d3d115554dfcb92069
naka0519 <naka20000519@gmail.com>
2024-05-23 11:58:20 +0900

    update app

--- app.py
+++ app.py
@@ -10,15 +10,25 @@ from datetime import datetime, timedelta, time
 # タイトル
 st.write("Suntory beacon data visualization")
 
-####### Input部分（選択でファイルpath指定）#######
+####### Data Input部分（選択でファイルpath指定）#######
 # csvを読み込む前提バージョン（事前に抽出が必要）
 # TODO: データベース連携
 Floor = st.selectbox("Floor", ["F1", "F2", "F3"])
 Person_Num = st.radio("Number of User", ["1", "2", "3"])
 User = st.selectbox("User", ["suzuki_7a", "nara_8a", "hamada_e4", "katou_79", "fujita_41"])
 # multi_select = st.multiselect("好きな色",options=["赤","青","黄"])
+######################
 
-# 可視化範囲
+# TODO: multiselectではlist方に対応が必要
+DataPath = f"./data/{Floor}_{User[-2:]}.csv"
+#DataPath = "./data/test.csv"
+try:
+    df = pd.read_csv(DataPath) # index: datatime
+    df["time"] = pd.to_datetime(df["time"]).dt.tz_localize(None) + timedelta(hours=9)
+except:
+    st.error("No data found. Please check the Floor or User Box.")
+
+###### 可視化範囲 Input部分 ######
 st.write("experimental period: 2023/9/10/18:00 - 2024/1/26/00:00")
 start_date = st.date_input(
     "Start date",
@@ -42,17 +52,7 @@ end_time = st.time_input(
 # st.write("Start time:", start_time)
 # st.write("End date:", end_date)
 # st.write("End time:", end_time)
-
 ######################
-# TODO: multiselectではlist方に対応が必要
-DataPath = f"./data/{Floor}_{User[-2:]}.csv"
-#DataPath = "./data/test.csv"
-
-try:
-    df = pd.read_csv(DataPath) # index: datatime
-    df["time"] = pd.to_datetime(df["time"]).dt.tz_localize(None) + timedelta(hours=9)
-except:
-    st.error("No data found. Please check the Floor or User Box.")
 
 # 可視化
 # TODO: 複数人のデータを一括可視化
