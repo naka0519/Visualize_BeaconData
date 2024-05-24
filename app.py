2d67174973f3b285023d5043d3e52332912ff036
naka0519 <naka20000519@gmail.com>
2024-05-24 10:17:18 +0900

    update app

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ except:
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 #fig.add_trace(go.Scatter(x=x, y=y, name=User), secondary_y=False)
 if Floor == "F1":
-    fig.add_trace(go.Scatter(x=x, y=df[["B_rssi", "L_rssi", "R_rssi"]], name="RSSI", mode="markers"), secondary_y=True)
+    fig.add_trace(go.Scatter(x=x, y=df[["B_rssi", "L_rssi", "R_rssi"]].values.tolist(), name="RSSI", mode="markers"), secondary_y=True)
 else:
-    fig.add_trace(go.Scatter(x=x, y=df[["B_rssi", "L_rssi", "F_rssi"]], name="RSSI", mode="markers"), secondary_y=True)
+    fig.add_trace(go.Scatter(x=x, y=df[["B_rssi", "L_rssi", "F_rssi"]].values.tolist(), name="RSSI", mode="markers"), secondary_y=True)
 #fig.update_yaxes(title_text="prediction", secondary_y=False)
 fig.update_yaxes(title_text="RSSI", secondary_y=True)
 fig.update_xaxes(rangeslider={"visible":True})
